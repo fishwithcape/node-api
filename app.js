7d293e278da8542ebe32f4fbac59db33abdc7035
fishwithcape <fishwithcape@github.com>
2021-02-24 23:39:40 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config')
+
+const app = express();
+
+
+// MiddleWare go here pls
+app.listen(3000, (err) => {
+    if (!err) {
+        console.log('Listening on 3000')
+    } else if (err) {
+        console.log('Error');
+    }
+});
+
+//mongodb connection
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (!err) {
+        console.log('Connected to Database')
+    } else {
+        return mongoose.Error
+    };
+});
+
+
